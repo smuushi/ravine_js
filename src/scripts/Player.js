11ae9bd967ba7104ddfc3d76023c397982fe5482
smuushi <mishih780@gmail.com>
2023-03-10 10:55:53 -0500

    player movement tune

--- src/scripts/Player.js
+++ src/scripts/Player.js
@@ -36,10 +36,15 @@ class Player {
 
         this.lastMovingDirection;
 
-        this.hitbox = new Hitbox(this.x, this.y, this.tileSize, this.tileSize, -1, -4)
+        this.hitbox = new Hitbox(this.x, this.y, this.tileSize - 3, this.tileSize - 1, 1, -4)
+
+        if (!Player.prototype.keyss){
+            Player.prototype.keyss = {}
+        }
 
     }
 
+    
 
     animate(ctx) {
         //288 x 480
@@ -99,8 +104,14 @@ class Player {
     }
 
 
-    _keydown =(event) => {
+    _keydown = (event) => {
         // debugger 
+        // console.log(event)
+        if (!Player.prototype.keyss[event.code]) {
+            Player.prototype.keyss[event.code] = event.code;
+            console.log(Player.prototype.keyss[event.code])
+        }
+
         if (event.key === 'ArrowUp'){
             this.currentMovingDirection = DIRS[0];
         } else if (event.key === 'ArrowDown'){
@@ -118,10 +129,42 @@ class Player {
         
     }
 
-    _keyup =(event) => {
+    _keyup = (event) => {
+        Player.prototype.keyss[event.code] = null;
+
+        console.log('key was let go')
         // console.log('up')
+        // this.lastLastMovingDirectionLmao = this.lastMovingDirection; // this doesn't work lol
+        // note here.. attempting to make movement more fluid.. 
+
+
         this.lastMovingDirection = this.currentMovingDirection;
+        
         this.currentMovingDirection = null;
+
+        if (Object.values(Player.prototype.keyss).some((val) => !!val)){
+            
+            let stillHeldKey;
+            Object.values(Player.prototype.keyss).forEach((value) => {
+                if (!!value) {
+                    stillHeldKey = value
+                }
+            })
+            // console.log(stillHeldKey)
+
+            if (stillHeldKey === 'ArrowUp'){
+                this.currentMovingDirection = DIRS[0];
+            } else if ((stillHeldKey) === 'ArrowDown'){
+                this.currentMovingDirection = DIRS[1];
+            } else if ((stillHeldKey)=== 'ArrowLeft'){
+                this.currentMovingDirection = DIRS[3];
+            } else if ((stillHeldKey) === 'ArrowRight'){
+                // debugger
+                this.currentMovingDirection = DIRS[2];
+            }
+
+            // console.log(this.currentMovingDirection)
+        }
     }
 
     
@@ -132,17 +175,48 @@ class Player {
 
     move() {
 
-        if (!!this.currentMovingDirection){
-            this.y = this.currentMovingDirection[1]* this.velocity + this.y;
-            this.x = this.currentMovingDirection[0]* this.velocity + this.x;
-            this.hitbox.x = this.x;
-            this.hitbox.y = this.y;
+        if (Object.values(Player.prototype.keyss).some((val) => !!val) ){
+
+            if (!!this.currentMovingDirection){
+
+                
+                // let oldX = this.x;
+                // let oldY = this.y;
+                
+                this.y = this.currentMovingDirection[1]* this.velocity + this.y; 
+                this.x = this.currentMovingDirection[0]* this.velocity + this.x;
+                this.hitbox.x = this.x;
+                this.hitbox.y = this.y;
+                Hitbox.updateCollisionStateToTrueIfColliding()
+                
+                while (this.hitbox.collisionState === true) {
+                    // debugger
+                    this.y = this.currentMovingDirection[1]*(-3)* this.velocity + this.y; 
+                    this.x = this.currentMovingDirection[0]*(-3)* this.velocity + this.x;
+                    this.hitbox.x = this.x;
+                    this.hitbox.y = this.y;
+                    Hitbox.updateCollisionStateToTrueIfColliding();
+
+                    // debugger
+                }
+                // if (this.hitbox.collisionState === true) {
+                //     // debugger
+                //     this.y = this.currentMovingDirection[1]*(-1)* this.velocity + this.y; 
+                //     this.x = this.currentMovingDirection[0]*(-1)* this.velocity + this.x;
+                //     this.hitbox.x = this.x;
+                //     this.hitbox.y = this.y;
+                //     // debugger
+                // }
+
+
+            }
+
+
+            // console.log([this.x, this.y])
+            // console.log(this.x)
+            // console.log(this.y)
+            // console.log(this.currentMovingDirection)
         }
-        // console.log([this.x, this.y])
-        // console.log(this.x)
-        // console.log(this.y)
-        // console.log(this.currentMovingDirection)
-        
     }
 
 }
